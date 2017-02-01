a8077be2272262505e78841f03a9806f2a89f211
jelitter <jelito@gmail.com>
2017-02-01 16:36:44 +0000

    Update WaterTank.java
    indentation

--- Programming 2 - SOFT6017/LabWeek 1 - Water Tank (MVC)/WaterTank.java
+++ Programming 2 - SOFT6017/LabWeek 1 - Water Tank (MVC)/WaterTank.java
@@ -1,51 +1,52 @@
-
 public class WaterTank {
 
-	private static final int INITIAL_VOLUME = 5000;
+  private static final int INITIAL_VOLUME = 5000;
 
-	public static void main(String[] args) {
+  public static void main(String[] args) {
 
-		int option = -1;
-		int litres = 0;
+    int option = -1;
+    int litres = 0;
 
-		WaterTankView myView = new WaterTankView();
-		WaterTankController myController = new WaterTankController(INITIAL_VOLUME);
+    WaterTankView myView = new WaterTankView();
+    WaterTankController myController = new WaterTankController(INITIAL_VOLUME);
 
 
-		while (option != 4) {
+    while (option != 4) {
 
-			myView.printMenu();
-			option = myView.readOption();	
+      myView.printMenu();
+      option = myView.readOption(); 
 
-			switch (option) {
-			case (1):
-				litres = myView.addWater();
-			if (myController.addWater(litres)) {
-				myView.print(litres + " litres added to Tank");	
-			} else {
-				myView.print("Couldn't add "+ litres + " litres, tank would overfill.");
-			}
-			
-			break;
-			case (2):
-				litres = myView.removeWater();
-			if (myController.removeWater(litres)) {
-				myView.print(litres + " litres removed from Tank");	
-			} else {
-				myView.print("Couldn't remove "+ litres + " litres, not enough water.");
-			}
-			
-			break;
-			case (3):
-				litres = myController.checkWater();
-			myView.print("There are " + litres + " litres in the tank");
-			break;
-			case (4):
-				myView.exitApp();
-			break;
-			default:
-				break;
-			}
-		}
-	}
+      switch (option) {
+        case (1):
+          litres = myView.addWater();
+          if (myController.addWater(litres)) {
+            myView.print(litres + " litres added to Tank"); 
+          } else {
+            myView.print("Couldn't add "+ litres + " litres, tank would overfill.");
+          }
+        break;
+
+        case (2):
+          litres = myView.removeWater();
+          if (myController.removeWater(litres)) {
+            myView.print(litres + " litres removed from Tank"); 
+          } else {
+            myView.print("Couldn't remove "+ litres + " litres, not enough water.");
+          }
+        break;
+
+        case (3):
+          litres = myController.checkWater();
+          myView.print("There are " + litres + " litres in the tank");
+        break;
+
+        case (4):
+          myView.exitApp();
+        break;
+
+        default:
+        break;
+      }
+    }
+  }
 }
