1e998bb582126742a8e0be48e616099b48b2bd6c
jelitter <jelito@gmail.com>
2017-02-01 16:37:35 +0000

    Update WaterTankController.java
    indentation

--- Programming 2 - SOFT6017/LabWeek 1 - Water Tank (MVC)/WaterTankController.java
+++ Programming 2 - SOFT6017/LabWeek 1 - Water Tank (MVC)/WaterTankController.java
@@ -1,33 +1,31 @@
-
 public class WaterTankController {
 
-	public final int MAX_VOLUME = 10000;
-	private int volume = 0;
-	
-	public WaterTankController(int l) {
-		volume = l;
-	}
-	
-	public boolean addWater(int n) {
-		if ((volume+n) < MAX_VOLUME) {
-			volume += n;
-			return true; // Added correctly.
-		} else {
-			return false; // Couldn't add over the limit.
-		}
-	}
-	
-	public boolean removeWater(int n) {
-		if ((volume-n) >= 0) {
-			volume -= n;
-			return true; // Water removed correctly.
-		} else {
-			return false; // Couldn't remove, not enough water.
-		}
-	}
-	
-	public int checkWater() {
-		return volume;
-	}	
+  public final int MAX_VOLUME = 10000;
+  private int volume = 0;
+  
+  public WaterTankController(int l) {
+    volume = l;
+  }
+  
+  public boolean addWater(int n) {
+    if ((volume+n) < MAX_VOLUME) {
+      volume += n;
+      return true; // Added correctly.
+    } else {
+      return false; // Couldn't add over the limit.
+    }
+  }
+  
+  public boolean removeWater(int n) {
+    if ((volume-n) >= 0) {
+      volume -= n;
+      return true; // Water removed correctly.
+    } else {
+      return false; // Couldn't remove, not enough water.
+    }
+  }
+  
+  public int checkWater() {
+    return volume;
+  } 
 }
-
